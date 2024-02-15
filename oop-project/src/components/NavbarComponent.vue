ad31c118760bf490080bcaec3dd4051345859a1c
ItsPeeko <111348072+ItsPeeko@users.noreply.github.com>
2024-02-15 12:52:23 +0800

    trying to set up vue homepage

---
+++ oop-project/src/components/NavbarComponent.vue
@@ -0,0 +1,53 @@
+<!-- src/components/NavbarComponent.vue -->
+<template>
+  <div>
+    <nav>
+        <ul>
+            <li>
+            <div class="home-icon">
+                <div class="roof">
+                <div class="roof-edge"></div>
+                </div>
+                <div class="front"></div>
+            </div>
+            </li>
+            <li>
+            <div class="about-icon">
+                <div class="head">
+                <div class="eyes"></div>
+                <div class="beard"></div>
+                </div>
+            </div>
+            </li>
+            <li>
+            <div class="work-icon">
+                <div class="paper"></div>
+                <div class="lines"></div>
+                <div class="lines"></div>
+                <div class="lines"></div>
+            </div>
+            </li>
+            <li>
+            <div class="mail-icon">
+                <div class="mail-base">
+                <div class="mail-top"></div>
+                </div>
+            </div>
+            </li>
+        </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavbarComponent', // Update the component name
+  // Component options
+}
+</script>
+
+<style>
+/* Component-specific styles */
+</style>
+
+
---
+++ oop-project/src/pages/HomepageView.vue
@@ -0,0 +1,22 @@
+<!-- src/pages/HomepageView.vue -->
+<template>
+  <div>
+    <NavbarComponent /> <!-- Include the NavbarComponent.vue component here -->
+    <!-- Homepage content -->
+  </div>
+</template>
+
+<script>
+import NavbarComponent from '../components/NavbarComponent.vue'; // Import the NavbarComponent.vue component
+
+export default {
+  name: 'HomepageView', // Update the component name
+  components: {
+    NavbarComponent // Register the NavbarComponent.vue component
+  }
+}
+</script>
+
+<style>
+/* Component-specific styles */
+</style>
