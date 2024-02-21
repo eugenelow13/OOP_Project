9be0f72867b8b343fedd16b9119a24a003a4f04a
seanl01 <57535001+seanl01@users.noreply.github.com>
2024-02-21 16:53:06 +0800

    User Entity Test

---
+++ backend/src/main/java/entity/User.java
@@ -0,0 +1,4 @@
+package entity;
+
+record User(Long id, String name, String email) {
+}
--- backend/src/main/java/model/HelloController.java
+++ backend/src/main/java/model/HelloController.java
@@ -1,3 +1,4 @@
+package model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
