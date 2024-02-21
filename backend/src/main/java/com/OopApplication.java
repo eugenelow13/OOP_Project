33e43fbbcffc04518ab7cfd99d56621f10011354
Eugene <eugene.low.2021@scis.smu.edu.sg>
2024-02-21 10:41:32 +0800

    Updated changes

--- backend/src/main/java/com/OopApplication.java
+++ backend/src/main/java/com/OopApplication.java
@@ -1,9 +1,11 @@
 package com;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "model")
 public class OopApplication {
 
 	public static void main(String[] args) {
--- backend/src/main/java/model/HelloController.java
+++ backend/src/main/java/model/HelloController.java
@@ -1,3 +1,4 @@
+package model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -6,6 +7,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello, World!";
+        return "Hello, World";
     }
 }
