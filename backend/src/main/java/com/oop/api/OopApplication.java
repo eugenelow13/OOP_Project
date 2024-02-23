92b9d78e1de21e1989456b9239c3b6b3bddefdd5
seanl01 <57535001+seanl01@users.noreply.github.com>
2024-02-23 22:43:35 +0800

    refactor: re-arrange annotations

--- backend/src/main/java/com/oop/api/OopApplication.java
+++ backend/src/main/java/com/oop/api/OopApplication.java
@@ -1,15 +1,13 @@
 package com.oop.api;
 
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication
 // @ComponentScan(basePackages = {"controller", "model"})
 // @EnableJpaRepositories(basePackages = "repository")
 // @EntityScan(basePackages = "model")
+@SpringBootApplication
 @ComponentScan
 public class OopApplication {
 
