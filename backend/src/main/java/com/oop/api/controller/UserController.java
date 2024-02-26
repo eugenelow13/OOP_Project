49096eb517d900fe954a5df011976a6a43278e76
seanl01 <57535001+seanl01@users.noreply.github.com>
2024-02-26 20:45:39 +0800

    change: implement user service

--- backend/src/main/java/com/oop/api/controller/UserController.java
+++ backend/src/main/java/com/oop/api/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.oop.api.controller;
 
+import static com.oop.api.util.ResponseHandler.generateResponseForOptional;
+
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatusCode;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,7 +16,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.oop.api.model.User;
-import com.oop.api.repository.UserRepository;
+import com.oop.api.service.UserService;
 import com.oop.api.util.ResponseHandler;
 
 import jakarta.validation.Valid;
@@ -23,23 +25,24 @@ import jakarta.validation.Valid;
 @RequestMapping("/users")
 public class UserController {
     @Autowired
-    private UserRepository userRepository;
+    // private UserRepository userRepository;
+    private UserService userService;
 
     @GetMapping(path = "")
     public @ResponseBody Iterable<User> getAllUsers() {
         // This returns a JSON or XML with the users
-        return userRepository.findAll();
+        return userService.getAllUsers();
     }
 
     @GetMapping(path = "/{id}")
     public @ResponseBody Optional<User> getUser(@PathVariable Integer id) {
         // This returns a JSON or XML with the users
-        return userRepository.findById(id);
+        return userService.getUserById(id);
     }
 
     @PostMapping(path = "")
     public ResponseEntity<Object> addNewUser(@Valid @RequestBody User user) {
-        userRepository.save(user);
-        return ResponseHandler.generateResponse("Created", (Object) user, HttpStatusCode.valueOf(200));
+        userService.addNewUser(user);
+        return ResponseHandler.generateResponse("Created", (Object) user, HttpStatus.OK);
     }
 }
