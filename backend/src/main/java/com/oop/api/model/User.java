82943cb3a3e9e297e4651628fa65a066a3a2b94e
seanl01 <57535001+seanl01@users.noreply.github.com>
2024-02-26 22:07:19 +0800

    chore: remove commented out getters and setters in User.java

--- backend/src/main/java/com/oop/api/model/User.java
+++ backend/src/main/java/com/oop/api/model/User.java
@@ -25,28 +25,4 @@ public class User {
   @Email
   @Column(name="email", unique=true)
   private String email;
-
-  // public Integer getId() {
-  //   return id;
-  // }
-
-  // public void setId(Integer id) {
-  //   this.id = id;
-  // }
-
-  // public String getName() {
-  //   return name;
-  // }
-
-  // public void setName(String name) {
-  //   this.name = name;
-  // }
-
-  // public String getEmail() {
-  //   return email;
-  // }
-
-  // public void setEmail(String email) {
-  //   this.email = email;
-  // }
 }
