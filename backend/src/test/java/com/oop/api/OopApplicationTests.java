ccf7945643b797227e70da573bb2cbdb19a600bd
seanl01 <57535001+seanl01@users.noreply.github.com>
2024-02-26 20:46:02 +0800

    test: implement some testing boilerplate

---
+++ backend/src/test/java/com/oop/api/OopApplicationTests.java
@@ -0,0 +1,19 @@
+package com.oop.api;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+// @SpringBootTest
+class OopApplicationTests {
+
+	// @Test
+	void contextLoads() {
+	}
+
+    @Test
+    void simpleTest() {
+        assertEquals(1, 1);
+    }
+
+}
---
+++ backend/src/test/java/com/oop/api/controller/UserControllerTest.java
@@ -0,0 +1,13 @@
+package com.oop.api.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+
+public class UserControllerTest {
+    @Test
+    void testHelloWorld() {
+        assertEquals(1, 1);
+    }
+}
