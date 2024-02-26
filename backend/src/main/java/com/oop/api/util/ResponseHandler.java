f11523c789b6aed39da5001ce384a768bb53f249
seanl01 <57535001+seanl01@users.noreply.github.com>
2024-02-26 20:48:03 +0800

    change: make message field in response optional

--- backend/src/main/java/com/oop/api/util/ResponseHandler.java
+++ backend/src/main/java/com/oop/api/util/ResponseHandler.java
@@ -2,15 +2,21 @@ package com.oop.api.util;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 
 public class ResponseHandler {
     public static ResponseEntity<Object> generateResponse(String message, Object data, HttpStatusCode status) {
         Map<String, Object> map = new HashMap<String, Object>();
-        map.put("message", message);
+        
+        if (message != null)
+            map.put("message", message);
+
         map.put("data", data);
         return new ResponseEntity<Object>(map, status);
     }
+
 }
