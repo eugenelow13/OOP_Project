92f7e425181f6709b8506dea8ec1e3d0645b5ffc
luqmanj <luqmanmn.2020@scis.smu.edu.sg>
2024-03-02 15:57:29 +0800

    Created Event.java

---
+++ backend/src/main/java/com/oop/api/model/Event.java
@@ -0,0 +1,46 @@
+package com.oop.api.model;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class Event {
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String type;
+
+    @NotNull
+    private String venue;
+
+    @NotNull
+    private LocalDateTime date;
+
+    private double ticketPrice;
+
+    private double cancellationFee;
+
+    private int noOfTicketsAvailable;
+
+    private int customerAttendance;
+
+}
