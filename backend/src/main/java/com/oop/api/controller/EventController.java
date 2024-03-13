8ff6955707cc2e4fa342d7983a0770b323fed4d5
seanl01 <57535001+seanl01@users.noreply.github.com>
2024-03-13 22:19:42 +0800

    change: edit events API routes, use PUT and PATCH methods, add mapper bean, remove findByName route.

--- backend/src/main/java/com/oop/api/controller/EventController.java
+++ backend/src/main/java/com/oop/api/controller/EventController.java
@@ -2,19 +2,14 @@ package com.oop.api.controller;
 
 import static com.oop.api.util.ResponseHandler.generateResponse;
 
-import java.util.Optional;
+import java.util.*;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.oop.api.model.Event;
 import com.oop.api.service.EventService;
@@ -29,36 +24,52 @@ public class EventController {
     @Autowired
     private EventService eventService;
 
+    @Autowired
+    private ModelMapper mapper;
+
+    private HashSet<String> patchableFields = new HashSet<>(Arrays.asList("name", "type", "venue", "date", "ticketPrice", "cancellationFee", "ticketsAvailable", "customerAttendance"));
+
     @GetMapping(path = "")
     public @ResponseBody ResponseEntity<Object> getAllEvents() {
         Iterable<Event> events = eventService.getAllEvents();
         return generateResponse(events);
     }
 
-    @GetMapping(path = "/findEvent")
-    public @ResponseBody Optional<Event> getEvent(@RequestParam String name) {
-        Optional<Event> event = eventService.getEventByName(name);
+    @GetMapping(path = "/{eventId}")
+    public @ResponseBody ResponseEntity<Object> getEvent(@PathVariable Integer eventId) {
+        Optional<Event> event = eventService.getEventById(eventId);
 
         if (event.isEmpty())
             throw new EntityNotFoundException("Event not found");
 
-        return event;
+        return generateResponse(event.get());
     }
+
+    // @GetMapping(path = "")
+    // public @ResponseBody Optional<Event> getEvent(@RequestParam String name) {
+    //     Optional<Event> event = eventService.getEventByName(name);
+
+    //     if (event.isEmpty())
+    //         throw new EntityNotFoundException("Event not found");
+
+    //     return event;
+    // }
 
     @PostMapping(path = "")
     public ResponseEntity<Object> addNewEvent(@Valid @RequestBody Event event) {
-
         try {
             eventService.addNewEvent(event);
         } catch (DataIntegrityViolationException e) {
-            return generateResponse("Event already exists. Please use a different ID.", (Object) event);
+            throw new DataIntegrityViolationException("Event already exists. Please use a different ID.");
         }
-
-        return generateResponse("Event is successfully created", (Object) event);
+        return generateResponse("Event is successfully created", (Object) event, HttpStatus.CREATED);
     }
 
-    @PostMapping(path = "/updateEvent")
-    public ResponseEntity<Object> updateEvent(@Valid @RequestBody Event updatedEvent) {
+    @PutMapping(path = "/{eventId}")
+    public ResponseEntity<Object> updateEvent(@PathVariable Integer eventId, @Valid @RequestBody Event updatedEvent) {
+        if (eventId != updatedEvent.getId())
+            throw new IllegalArgumentException("Event ID in the path and request body do not match");
+
         try {
             Event result = eventService.updateEvent(updatedEvent);
             return generateResponse("Event is successfully updated", result);
@@ -67,34 +78,18 @@ public class EventController {
         }
     }
 
-    @PostMapping("/setCancellationFee")
-    public ResponseEntity<Object> setCancellationFee(@RequestParam(name = "eventId") Integer eventId, @RequestParam(name = "newCancellationFee") double newCancellationFee) {
-        try {
-            Event updatedEvent = eventService.setCancellationFee(eventId, newCancellationFee);
-            return generateResponse("Cancellation fee updated successfully", updatedEvent);
-        } catch (EntityNotFoundException e) {
-            return generateResponse("Event not found", null);
-        }
+    // take in updatedEvent
+    @PatchMapping("/{eventId}")
+    public ResponseEntity<Object> patchEvent(@PathVariable Integer eventId, @RequestBody Map<String, Object> eventPatch) {
+        Event event = eventService.getEventById(eventId)
+                .orElseThrow(() -> new EntityNotFoundException("Event not found"));
+    
+        if (eventPatch.keySet().stream().anyMatch(k -> !patchableFields.contains(k)))
+            throw new IllegalArgumentException("Invalid fields provided in request body");
+
+        mapper.map(eventPatch, event);
+    
+        return updateEvent(event.getId(), event);
     }
 
-    @PostMapping("/updateAttendance")
-    public ResponseEntity<Object> updateAttendence(@RequestParam(name = "eventId") Integer eventId, @RequestParam(name = "newAttendance") Integer newAttendance) {
-        try {
-            Event updatedAttendance = eventService.updateAttendence(eventId, newAttendance);
-            return generateResponse("Attendance updated successfully", updatedAttendance);
-        } catch (EntityNotFoundException e) {
-            return generateResponse("Event not found", null);
-        }
-    }
-
-    @PostMapping("/updateTicketsAvailable")
-    public ResponseEntity<Object> updateTicketsAvailable(@RequestParam(name = "eventId") Integer eventId, @RequestParam(name = "newTicketsAvailable") Integer newTicketsAvailable) {
-        try {
-            Event updatedAttendance = eventService.updateTicketsAvailable(eventId, newTicketsAvailable);
-            return generateResponse("Attendance updated successfully", updatedAttendance);
-        } catch (EntityNotFoundException e) {
-            return generateResponse("Event not found", null);
-    }
-
-}
 }
