ed11d3ecffbb64bf38e79e4b4a2b256c91d7de20
CHUA WEI LING, JACQUELINE _ <wlchua.2021@scis.smu.edu.sg>
2024-03-14 02:38:53 +0800

    created login and signup

---
+++ backend/src/main/java/com/oop/api/service/AuthenticationService.java
@@ -0,0 +1,51 @@
+package com.oop.api.service;
+
+import com.oop.api.dto.LoginUserDTO;
+import com.oop.api.dto.RegisterUserDTO;
+import com.oop.api.model.User;
+import com.oop.api.repository.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+
+public class AuthenticationService {
+
+    private final UserRepository userRepository;
+    
+    private final PasswordEncoder passwordEncoder;
+    
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationService(
+        UserRepository userRepository,
+        AuthenticationManager authenticationManager,
+        PasswordEncoder passwordEncoder
+    ) {
+        this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User signup(RegisterUserDTO input) {
+        User user = new User();
+        user.setFullName(input.getFullName());
+        user.setEmail(input.getEmail());
+        user.setPassword(passwordEncoder.encode(input.getPassword()));
+
+        return userRepository.save(user);
+    }
+
+    public User authenticate(LoginUserDTO input) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        input.getEmail(),
+                        input.getPassword()
+                )
+        );
+
+        return userRepository.findByEmail(input.getEmail())
+                .orElseThrow();
+    }
+}
