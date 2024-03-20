6b3e2bf2995280c0e943015c52873fc393782d9a
CHUA WEI LING, JACQUELINE _ <wlchua.2021@scis.smu.edu.sg>
2024-03-21 07:15:09 +0800

    cleaned up

--- backend/src/main/java/com/oop/api/controller/EventController.java
+++ backend/src/main/java/com/oop/api/controller/EventController.java
@@ -6,7 +6,6 @@ import java.util.*;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
