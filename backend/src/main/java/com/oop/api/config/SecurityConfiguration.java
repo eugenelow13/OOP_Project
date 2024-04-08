e18a3d3a0535c4bae24262fe459f47dc79e6533b
nasingham <nasingham.2021@scis.smu.edu.sg>
2024-04-08 10:02:28 +0800

    fix

--- backend/src/main/java/com/oop/api/config/SecurityConfiguration.java
+++ backend/src/main/java/com/oop/api/config/SecurityConfiguration.java
@@ -60,7 +60,7 @@ public class SecurityConfiguration {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(List.of("http://localhost:8082"));
-        configuration.setAllowedMethods(List.of("GET","POST"));
+        configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization","Content-Type"));
         configuration.addExposedHeader("Access-Control-Allow-Origin");
 
